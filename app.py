2f9e4706bb997f3ff3125ce44701aa85a4f274d7
ibrahimmealassestantashti-dotcom <ibrahim.meal.assestant.ashti@gmail.com>
2026-07-30 13:09:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -247,12 +247,13 @@ with st.sidebar:
     st.header("🔑 إعدادات الذكاء الاصطناعي")
     user_gemini_key = st.text_input("مفتاح Gemini API", type="password", value=st.secrets.get("GEMINI_API_KEY", ""))
     
-    free_models_options = [
+   free_models_options = [
+        "gemini-3.6-flash",
+        "gemini-3.5-flash",
+        "gemini-3.1-pro-preview",
         "gemini-2.5-flash",
-        "gemini-2.0-flash",
         "gemini-2.5-pro",
-        "gemini-1.5-flash",
-        "gemini-1.5-pro"
+        "gemini-2.0-flash"
     ]
     
     selected_ai_model = st.selectbox("اختر نموذج الذكاء الاصطناعي:", free_models_options, index=0)
